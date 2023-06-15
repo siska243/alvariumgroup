37a94d0b77642e03f151abb7387588c393317db0
Emmanuel Simisi <emmanuelsimisi@gmail.com>
2023-06-15 11:02:22 +0100

    📦 NEW: paginator with knp

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,19 +3,27 @@
 namespace App\Controller;
 
 use App\Repository\OffreEmploiRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/",name="app_home",methods={"GET"})
      */
-    public function index(OffreEmploiRepository $offreEmploiRepository): Response
+    public function index(OffreEmploiRepository $offreEmploiRepository,PaginatorInterface $paginator, Request $request): Response
     {
+        $offres = $offreEmploiRepository->findAll();
+        $offre_emplois = $paginator->paginate(
+            $offres, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            10 // Nombre de résultats par page
+        );
         return $this->render('home/index.html.twig', [
-            'offres' => $offreEmploiRepository->findAll(),
+            'offres' => $offre_emplois,
         ]);
     }
     /**
